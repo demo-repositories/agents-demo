4beddb1651c6dc65e1ed73e38b73ffd627eff0c5
ChrisLaRocque <larocque.christopher@gmail.com>
2025-05-15 22:19:33 -0400

    wut

--- apps/studio/functions/translate/index.ts
+++ apps/studio/functions/translate/index.ts
@@ -1,11 +1,18 @@
+import {translate} from '../../lib/translate'
+
 export async function handler({context, event}) {
   const time = new Date().toLocaleTimeString()
   console.log(`👋 Your Sanity Function was called at ${time}`)
   console.log(context)
   if (
-    !context ||
-    !context.clientOptions.projectId ||
-    context.clientOptions.projectId !== 'blneb7aj'
-  )
+    !context
+    // !context.clientOptions.projectId ||
+    // context.clientOptions.projectId !== 'blneb7aj'
+  ) {
+    console.log('returning')
     return
+  }
+  if (event.data) {
+    await translate({...event.data, id: event.data._id})
+  }
 }
