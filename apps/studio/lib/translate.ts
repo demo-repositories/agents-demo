2e924c3a471a8b6e3de5f8b941cb360a08649ec2
ChrisLaRocque <larocque.christopher@gmail.com>
2025-05-16 09:55:39 -0400

    Recreate large bundle

--- apps/studio/lib/translate.ts
+++ apps/studio/lib/translate.ts
@@ -1,5 +1,5 @@
 import type {SanityClient, SanityDocument} from 'sanity'
-import {supportedLanguages} from '../i18n'
+import {supportedLanguages} from '../sanity.config'
 import {token, projectId, dataset} from '../env'
 import {createClient} from '@sanity/client'
 
